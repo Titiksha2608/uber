ff9640902d7d8c9df5046b8b8646e1556f8ae508
Titiksha <titikshamuk@gmail.com>
2025-01-24 16:24:34 +0530

    commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -23,3 +23,29 @@ const token = usergenerateAuthToken();
 
 res.status(201).json({token,user});
 }
+
+module.exports.loginUser = async(req,res,next)=>{
+    const errors = validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    
+    const {email,password} = req.body;  // get email and password from request body
+    
+    const user = await userModel.findOne({email}).select('+password');  // to get password
+
+    if(!user){
+        return res.status(401).json({message:'Invalid email or password'}); // if email is wrong
+    }
+
+
+    const isMatch = await user.comparePassword(password); //method from user.model.js
+    
+    if(!isMatch){
+        return res.status(401).json({message:'Invalid email or password'}); // if password is wrong
+    }
+
+    const token = user.generateAuthToken(); //method from user.model.js
+
+    res.status(200).json({token,user}); // send token and user details
+}
